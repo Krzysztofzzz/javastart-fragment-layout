8968f03b43ef26c2f9c176cbe38ad983942de849
Krzysztof <krzubrycki@gmail.com>
2022-03-08 16:06:22 +0100

    created project

---
+++ src/main/java/com/javastart/JavastartFragmentLayoutApplication.java
@@ -0,0 +1,13 @@
+package com.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartFragmentLayoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartFragmentLayoutApplication.class, args);
+    }
+
+}
